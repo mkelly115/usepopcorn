1347366a6ca65a1a4af926962f6a5d1283e2f229
Michael <mikek222@gmail.com>
2025-08-27 13:52:34 -0400

    restarted project with API call (wrong first)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
+import './index.css';
 // import App from './App';
 import StarRating from "./StarRating";
+import App1 from "./App copy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App1 />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={23} color="red" defaultRating={3} />
+    <StarRating size={23} color="red" defaultRating={3} /> */}
   </React.StrictMode>
 );
