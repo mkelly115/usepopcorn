57eb1f08606e34b98255f7ec61332d1ad14bd7cf
Michael <mikek222@gmail.com>
2025-10-09 16:20:19 -0400

    cleaned up app files, need to modulate each individual component"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
-// import App from './App';
-import StarRating from "./StarRating";
-import App1 from "./App copy";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App1 />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={23} color="red" defaultRating={3} /> */}
+    <App />
+
   </React.StrictMode>
 );
